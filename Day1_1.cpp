5ff45566c4f3ede6391122e91a766cb6ad05b2bc
nawaazish <124172426+nawaazish@users.noreply.github.com>
2024-03-27 22:04:14 +0530

    Create Day1_1.cpp

---
+++ Day1_1.cpp
@@ -0,0 +1,16 @@
+//Date:27-03-2024
+//Problem:https://www.codechef.com/START127D/problems/IDESM
+//Author:Ishita
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n==15){
+	    cout<<"Yes"<<endl;
+	    return 0;
+	}
+    cout<<"No"<<endl;
+	return 0;
+}
